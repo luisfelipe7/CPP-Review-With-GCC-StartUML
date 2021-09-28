1e60406d0070fe210853c5140556b2b403182fe1
Felipe Castro <luisfelipecs97@gmail.com>
2021-09-28 11:25:30 -0600

    Modified the Main to Implement a Menu

--- Main.cpp
+++ Main.cpp
@@ -24,27 +24,66 @@ int main()
     int birthYear = 0;
     int birthMonth = 0;
     string ownerId = "";
+    // While Option
+    bool havingUser = true;
+    // Menu Option
+    int option = 0;
+    string userAnswer = "";
+
+    // Declaring the Cat to save the values
+    Cat cat1;
 
     // Introduction and Requesting the necessary data
-    cout << "Welcome to F-Veterinary" << endl;
-    cout << "What is the name of your cat?" << endl;
-    cin >> name;
-    cout << "Thanks, could you please give us the birth date of your cat?" << endl;
-    cout << "1. The birth day first:" << endl;
-    cin >> birthDay;
-    cout << "2. The month now:" << endl;
-    cin >> birthMonth;
-    cout << "3. And the year:" << endl;
-    cin >> birthYear;
-    cout << "Perfect, now we just need the ID of the person that you want to register as the owner:" << endl;
-    cin >> ownerId;
+    while (havingUser)
+    {
+        cout << "-------------- Welcome to F-Veterinary --------------" << endl;
+        cout << "How can we help you? Please enter an option" << endl;
+        cout << "1. I need to register a cat." << endl;
+        cout << "2. Check if my cat needs a vaccine." << endl;
+        cin >> option;
 
-    // Creating the object
-    Cat cat1(name, birthMonth, birthDay, birthYear, ownerId);
-    cat1.displayCatInformation();
-    system("pause");
+        switch (option)
+        {
+        case 1:
+            system("cls");
+            cout << "What is the name of your cat?" << endl;
+            cin >> name;
+            cout << "Thanks, could you please give us the birth date of your cat?" << endl;
+            cout << "1. The birth day first:" << endl;
+            cin >> birthDay;
+            cout << "2. The month now:" << endl;
+            cin >> birthMonth;
+            cout << "3. And the year:" << endl;
+            cin >> birthYear;
+            cout << "Perfect, now we just need the ID of the person that you want to register as the owner:" << endl;
+            cin >> ownerId;
+            cat1.setName(name);
+            cat1.setBirthDate(birthMonth, birthDay, birthYear);
+            cat1.setOwnerId(ownerId);
+            cat1.displayCatInformation();
+            cout << endl;
+            system("pause");
+            cout << endl;
+            break;
+        case 2:
+            // Pending to Implement
+            break;
+        default:
+            cout << "Please enter a valid option" << endl;
+            break;
+        }
+
+        system("cls");
+
+        cout << "Do you need anything else?" << endl;
+        cin >> userAnswer;
+        (userAnswer == "No" || userAnswer == "no") ? havingUser = false : havingUser = true;
+    }
+
     cout << endl;
     cout << "Thanks for visiting us, take care!" << endl;
+    cout << endl;
+    system("pause");
 
     return 0;
 }
